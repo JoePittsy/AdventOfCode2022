139430b58d0ddd4ddcfbb015c674cdee929e9207
Joe Pitts <JPitts@TrueNorthIT.co.uk>
2022-12-07 10:47:16 +0000

    Day 7

---
+++ DaySeven/ElfNode.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaySeven
+{
+    public abstract class ElfNode
+    {
+        public ElfFolder? Parent { get; }
+        public string Name { get; }
+
+        public ElfNode(ElfFolder? parent, string name)
+        {
+            this.Parent = parent;
+            this.Name = name;
+        }
+        public abstract void PrintSelf(int depth = 0);
+
+    }
+
+    public class ElfFile: ElfNode
+    {
+        public int Size { get; }
+
+        public ElfFile(ElfFolder parent, string name, int size): base (parent, name)
+        {
+            this.Size = size;
+        }
+
+        public override void PrintSelf(int depth = 0)
+        {
+            for (int i = 0; i < depth; i++) { Console.Write(" "); }
+            Console.WriteLine($"- {Name} (file, size={Size})");
+        }
+    }
+
+    public class ElfFolder : ElfNode
+    {
+        public List<ElfNode> Children { get; } = new List<ElfNode>();
+        public ElfFolder(ElfFolder? parent, string name) : base(parent, name) { }
+        public int TotalSize { get; set; } = 0;
+
+        public bool SmallFolder = true;
+
+        public bool ContainsChildWith(string name)
+        {
+            return Children.Any(child => child.Name == name);
+        }
+        public ElfNode GetFolder(string name)
+        {
+            return Children.First(child => child.Name == name);
+        }
+
+        public void UpdateSize(int addition)
+        {
+            TotalSize += addition;
+            if (TotalSize >= 100000) SmallFolder = false;
+            if (Parent != null) Parent.UpdateSize(addition);
+        }
+
+        public void AddChild (ElfNode child)
+        {
+            if (ContainsChildWith(child.Name))
+            {
+                Console.Write($"Folder {Name} already has a child with path {child.Name}");
+            }
+            else
+            {
+                if (child.GetType() == typeof(ElfFile)) { UpdateSize(((ElfFile)child).Size); }
+                Children.Add(child);
+            }
+        }
+
+        public List<ElfFolder> GetSmallChildren()
+        {
+            List<ElfFolder> smallFolders = new List<ElfFolder>();
+            foreach (var child in Children)
+            {
+                if (child.GetType() == typeof(ElfFile)) continue;
+                var folder = (ElfFolder)child;
+                if (folder.SmallFolder) smallFolders.Add(folder);
+                var childsSmallFolders = folder.GetSmallChildren();
+                smallFolders.AddRange(childsSmallFolders);
+            }
+            return smallFolders;
+            }
+
+        public List<ElfFolder> GetAllChildren()
+        {
+            List<ElfFolder> allFolders = new List<ElfFolder>();
+            foreach (var child in Children)
+            {
+                if (child.GetType() == typeof(ElfFile)) continue;
+                var folder = (ElfFolder)child;
+                allFolders.Add(folder);
+                var childsFolders = folder.GetAllChildren();
+                allFolders.AddRange(childsFolders);
+            }
+            return allFolders;
+        }
+
+        public override void PrintSelf(int depth = 0)
+        {
+            for (int i = 0; i < depth; i++) { Console.Write(" "); }
+            if (SmallFolder) Console.WriteLine($"- {Name} (dir, GOLDEN!, TotalSize={TotalSize})");
+            else Console.WriteLine($"- {Name} (dir, TotalSize={TotalSize})");
+            foreach (var child in Children)
+            {
+                child.PrintSelf(depth+2);
+            }
+        }
+    }
+}
---
+++ DaySeven/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using static SharedFunctions.Shared;
+namespace DaySeven
+{
+    public static class Program
+    {
+
+        public static Tuple<int, int> partOneAndTwo(string inputPath)
+        {
+            var input = File.ReadAllLines(inputPath);
+            ElfFolder baseDir = new ElfFolder(null, "/");
+
+            ElfFolder currentDir = baseDir;
+
+            string currentCommand = "";
+
+            // Skip one as the first command is to enter the / dir
+            foreach (var line in input.Skip(1))
+            {
+                bool isCommand = line[0] == '$';
+                if (isCommand)
+                {
+                    if (line.StartsWith("$ ls")) { currentCommand = "ls";  }
+                    if (line.StartsWith("$ cd"))
+                    {
+                        currentCommand = "cd";
+                        var dir = line.Replace("$ cd ", "");
+                        if (dir == "..") { currentDir = currentDir.Parent; }
+                        else { currentDir = (ElfFolder)currentDir.GetFolder(dir); }
+                    }
+                }
+                else if (currentCommand == "ls")
+                {
+                    // Folder
+                    if (line[0] == 'd')
+                    {
+                        currentDir.AddChild(new ElfFolder(currentDir, line.Replace("dir ", "")));
+                    }
+                    // File
+                    else
+                    {
+                        var sizeName = line.Split(" ");
+                        currentDir.AddChild(new ElfFile(currentDir, sizeName[1], int.Parse(sizeName[0])));
+                    }
+                }
+            }
+
+            baseDir.PrintSelf(0);
+            var smallFolders = baseDir.GetSmallChildren();
+            var partOne = smallFolders.Sum(folder => folder.TotalSize);
+
+            var totalDiskSpace = 70000000;
+            var updateSpace = 30000000;
+            var usedSpace = baseDir.TotalSize;
+            var spaceRemaining = totalDiskSpace - usedSpace;
+            var neededSpace = updateSpace - spaceRemaining;
+            Console.WriteLine($"We need to free up {neededSpace}!");
+
+            var allFolders = baseDir.GetAllChildren();
+            var folderSizes = allFolders.FindAll(folder => folder.TotalSize >= neededSpace).Select(folder => folder.TotalSize).ToList();
+            folderSizes.Sort();
+            return Tuple.Create(partOne, folderSizes[0]);
+
+        }
+
+       
+        public static void Main()
+        {
+            var example = partOneAndTwo(@"C:\Users\Joe\Documents\Repos\Personal\AdventOfCode2022\AdventOfCode2022\DaySeven\example.txt");
+            Debug.Assert(example.Item1 == 95437);
+            Debug.Assert(example.Item2 == 24933642);
+
+
+            var actual = partOneAndTwo(@"C:\Users\Joe\Documents\Repos\Personal\AdventOfCode2022\AdventOfCode2022\DaySeven\real.txt");
+            Console.WriteLine($"Part One = {actual.Item1} Part Two = {actual.Item2}");
+        }
+    }
+}
